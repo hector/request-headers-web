b29cd2f4da8792efebad0e7ae28a199624dedb03
Hector Parra <hector@parra.cat>
2024-07-04 19:12:48 +0200

    Sort headers

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   const headersList = headers();
   const headersArray = Array.from(headersList.entries());
 
+  // Sort headersArray by key
+  headersArray.sort(([keyA], [keyB]) => keyA.localeCompare(keyB));
+
   return (
     <div>
       <h1>Request Headers</h1>
