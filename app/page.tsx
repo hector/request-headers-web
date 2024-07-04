0614d66b56d68873bd9066a2398d7f59163cf7b2
Hector Parra <hector@parra.cat>
2024-07-04 19:08:21 +0200

    Print request headers

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,19 @@
-/** Add your relevant code here for the issue to reproduce */
+import { headers } from 'next/headers';
+
 export default function Home() {
-  return null;
+  const headersList = headers();
+  const headersArray = Array.from(headersList.entries());
+
+  return (
+    <div>
+      <h1>Request Headers</h1>
+      <ul>
+        {headersArray.map(([key, value]) => (
+          <li key={key}>
+            <strong>{key}:</strong> {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
